01bac425b4f791b8734655c9d9b358d05d179f43
subibiclientjohn <80508414+subibiclientjohn@users.noreply.github.com>
2022-04-09 20:16:24 +0800

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+ namespace App\Models;
+ 
+ use Illuminate\Database\Eloquent\Model;
+ class User extends Model{
+ protected $table = 'tbluser';
+ // column sa table
+ protected $fillable = [
+ 'user', 'password'
+ ];
+ }
